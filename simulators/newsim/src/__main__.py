d2426093db6f9e1789b5b8d0b6411e35433cfb3d
Dane <dane.andrews99@gmail.com>
2019-02-17 12:45:26 -0500

    Sim is coming along

--- simulators/newsim/src/__main__.py
+++ simulators/newsim/src/__main__.py
@@ -1,76 +1,90 @@
+# Hola, and welcome to the simulator
+# this file contains a lot of dependencies, init stuff,
+# and lcm interfacing. if you're looking for underlying
+# code as to how the simulator initializes, this file
+# is for you. if not, check other files
+
+# CGI AND THREADING (as a reminder)
+# Structural notes:
+# So I'm thinking we should initialize lcm and all sim
+# objects in this file, as well as their associated methods,
+# however we also should make sure this file doesn't get too
+# long and unmanagable, which is why I'd like to write the
+# high-level functionality in simHandler.py to make it easier
+# to manage and debug
+from . import simHandler
 from rover_common import aiolcm
+from abc import ABC
+import math  # , abstractmethod
+# import threading  # for later for perf improvements
+# import time  # for later, for more accurate information and logging
 import asyncio
 from rover_common.aiohelper import run_coroutines
 # from rover_msgs import DanesMsg
-from rover_msgs import NavStatus
-from rover_msgs import Joystick
-from rover_msgs import Bearing
-from rover_msgs import AutonState
-from rover_msgs import GPS
+from rover_msgs import (NavStatus, Joystick, Odometry, AutonState,
+                        Course, Obstacle, TennisBall)
+
+# per Faruk's advice, the simulator is now contained in a metaclass
 
 
-# async def publish_nav_state(lcm):
-#     while True:
-#         msg = nav_state()
-#         msg.
-
-
-class LCMInterface:
+class SimulatorMetaClass:
 
     # variables defined here are common to all classes
     # ideally it shouldn't matter bc we only ever need one instance
     # this is bad practice imho, just defined vars in the block below
     def __init__(self):  # other args if ya want em
         # all initially defined variables should be here
-        # this is effectively the class ctor, made of classes of messages
-        # use according to https://docs.python.org/3/tutorial/classes.html
-        # self.var = var_val (from __init__ input)
-        # variables defined here are unique to each instance of the class
-        # self.heres_a_var = "i'm a var"
+        # while not technically globals, this is basically where they are
+        # defined for the sim, since the entire instance is the SimMetaClass
+
+        Obstacles = []
+        Waypoints = []
+        Tennis_Balls = []
 
         # below: class list, one class for each message type
         # published or recieved. instantiate them at the bottom
         # of this message definition block
-        class NavStatusClass:
-            nav_state = 0
-            completed_wps = 0
-            missed_wps = 0
-            total_wps = 0
+        # use the provided imported classes and dump these later
+        # you still need to set all the defaults
 
-        class JoystickClass:
-            foward_back = 0
-            left_right = 0
+        self.NavStatusMsg = NavStatus()
+        self.JoystickMsg = Joystick()
+        # self.GPSMsg = GPS()
+        # self.BearingMsg = Bearing()
+        self.OdomMsg = Odometry()
+        self.ObstacleMsg = Obstacle()
+        self.TennisBallMsg = TennisBall()
+        self.CourseMsg = Course()
+        self.AutonStateMsg = AutonState()
 
-        class GPSClass:
-            latitude_deg = 39.4
-            latitude_min = 0
-            longitude_deg = -110.5
-            longitude_min = 0
+        self.NavStatusMsg.nav_state = 0
+        self.NavStatusMsg.completed_wps = 0
+        self.NavStatusMsg.missed_wps = 0
+        self.NavStatusMsg.total_wps = 0
 
-        class BearingClass:
-            bearing = 0.0
+        self.JoystickMsg.forward_back = 0
+        self.JoystickMsg.left_right = 0
 
-        class ObstacleClass:
-            rocks = []
+        self.OdomMsg.latitude_deg = 39
+        self.OdomMsg.latitude_min = 0
+        self.OdomMsg.longitude_deg = -110
+        self.OdomMsg.longitude_min = 0
+        self.OdomMsg.bearing = 0
+        self.OdomMsg.speed = -999  # this value is never used
+        # so it's being set to a dummy value. DO NOT USE IT
 
-        class TennisBallClass:
-            tennis_balls = []
+        self.ObstacleMsg.detected = 0
+        self.ObstacleMsg.bearing = 0
 
-        class CourseClass:
-            waypoints = []
+        self.TennisBallMsg.found = 0
+        self.TennisBallMsg.bearing = 0
+        self.TennisBallMsg.distance = 0
 
-        class AutonStateClass:
-            is_auton = False
+        self.CourseMsg.num_waypoints = 0
+        self.CourseMsg.hash = 0
+        self.CourseMsg.waypoints = []
 
-        self.NavStatusMsg = NavStatusClass()
-        self.JoystickMsg = JoystickClass()
-        self.GPSMsg = GPSClass()
-        self.BearingMsg = BearingClass()
-        self.ObstacleMsg = ObstacleClass()
-        self.TennisBallMsg = TennisBallClass()
-        self.CourseMsg = CourseClass()
-        self.AutonStateMsg = AutonStateClass()
-
+        self.AutonStateMsg.is_auton = False
     # definitions for message processing are below, with callbacks (cb)
     # at the top and publishing at the bottom
     # in this setup, camelCasing denotes a class instance
@@ -92,82 +106,136 @@ class LCMInterface:
         self.JoystickMsg.kill = m.kill
         self.JoystickMsg.restart = m.restart
 
-    async def publish_bearing(self, lcm):
-        while True:
-            msg = Bearing()
-            msg.bearing = self.BearingMsg.bearing
-            lcm.publish("\bearing", msg.encode())
-            await asyncio.sleep(10)
+    # async def publish_bearing(self, lcm):
+    #     while True:
+    #         lcm.publish("\bearing", self.BearingMsg.encode())
+    #         await asyncio.sleep(10)
 
     async def publish_auton_state(self, lcm):
         while True:
-            msg = AutonState()
-            msg.is_auton = self.AutonStateMsg.is_auton
-            lcm.publish("\auton", msg.encode())
+            lcm.publish("\auton", self.AutonStateMsg.encode())
             await asyncio.sleep(10)
 
-    async def publish_gps_state(self, lcm):
-        while True:
-            msg = GPS()
-            msg.latitude_deg = self.GPSMsg.latitude_deg
-            msg.latitude_min = self.GPSMsg.latitude_min
-            msg.longitude_deg = self.GPSMsg.longitude_deg
-            msg.longitude_min = self.GPSMsg.longitude_min
-            lcm.publish("\GPS", msg.encode())
-            await asyncio.sleep(10)
-    # test message code as a reference for subscription/publishing
-    # with a simple intermediate operation
-
-    # def sampleMethod(self):
-    #   self.heres_a_var = self.heres_a_var + " again"
-
-    # def test_cb(self, channel, msg):
-    #     m = DanesMsg.decode(msg)
-    #     self.heres_a_var = m.test_roundtrip
-    #     print(self.heres_a_var)
-    #     self.sampleMethod()
-
-    # async def publish_test(self, lcm):
+    # async def publish_gps_state(self, lcm):
     #     while True:
-    #         msg = DanesMsg()
-    #         msg.test_var_code = 2
-    #         msg.test_roundtrip = self.heres_a_var
-    #         lcm.publish("\test", msg.encode())
-    #         await asyncio.sleep(1000)
+    #         lcm.publish("\GPS", self.GPSMsg.encode())
+    #         await asyncio.sleep(10)
 
-    # definitions of other methods should go below here
+    async def publish_odom(self, lcm):
+        while True:
+            lcm.publish("\odometry", self.OdomMsg.encode())
+            await asyncio.sleep(10)
+
+    async def publish_course(self, lcm):
+        while True:
+            lcm.publish("\course", self.CourseMsg.encode())
+            await asyncio.sleep(10)
+
+    async def publish_obstacle(self, lcm):
+        while True:
+            lcm.publish("\obstacle", self.ObstacleMsg.encode())
+            await asyncio.sleep(10)
+
+    async def publish_tennis_ball(self, lcm):
+        while True:
+            lcm.publish("\tennis_ball", self.TennisBallMsg.encode())
+            await asyncio.sleep(10)
+
+    # SimObject definitions are below
+    # SimObj is the abstract base class that contains properties
+    # common to all objects. define additional simulator objects
+    # as if you would the Rover class, including proper
+    # superclass init
+
+    # identical to the odom message, minus speed, bc it's a useful
+    # object to have internally
+    class Odom:
+        def __init__(self, lat0, latm0, lon0, lonm0, bearing):
+            self.lat_deg = lat0
+            self.lat_min = latm0
+            self.lon_deg = lon0
+            self.lon_min = lonm0
+            self.bearing = bearing
+            self.speed = -999
+
+    # parent class of sim objects. Has all properties common to all
+    # objects
+
+    class SimObj(ABC):
+        # define initial location and other properties
+        def __init__(self, odom):
+            self.lat_deg = odom.lat0
+            self.lat_min = odom.latm0
+            self.lon_deg = odom.lon0
+            self.lon_min = odom.lonm0
+            self.bearing = odom.bearing0
+            self.shape = 0  # need to create a seed system?
+
+        # any methods common to all classes should be defined
+        def get_coords(self):
+            return [self.lat_deg, self.lat_min,
+                    self.lon_deg, self.lon_min]
+
+        def get_bearing(self):
+            return self.bearing
+
+        # here is an abstract method, may be useful
+        # @abstractmethod
+        # def sample_abs_method(self):
+        #     pass
+    class Field(SimObj):
+        def __init__(self, odom, radius=2):  # other properties
+            super().__init__(odom)
+            self.radius = radius  # in degrees, if not specified
+            # radius is 2
+
+    class Rover(SimObj):
+        def __init__(self, odom, speed_trans=1,
+                     speed_rot=1):
+            super().__init__(odom)
+            self.fov = 120  # units of degrees,
+            # 120 if not specified
+            self.cv_thresh = 5
+            self.speed_translational = speed_trans
+            # speed multiplier, 1 if not specified
+            self.speed_rotational = speed_rot
+
+    class TennisBall(SimObj):
+        def __init__(self, odom):  # other properties
+            super().__init__(odom)
+            self.other_prop = 0
+
+    class Obstacle(SimObj):
+        def __init__(self, odom):  # other properties
+            super().__init__(odom)
+
+    class Waypoint(SimObj):
+        def __init__(self, odom, searchable=0):
+            super().__init__(odom)
+            self.search = searchable  # defaults to false if not set
 
 
 def main():
     # how you get lcm messages
     lcm = aiolcm.AsyncLCM()
 
-    # instantiates LCMInterface class
-    interface = LCMInterface()
-
-    # message subscriptions should appear below
-    # lcm.subscribe("\test", interface.test_cb)
-    # uncomment this code to see test code of lcm subscription/publishing
-    # with the associated callbacks
-
-    # don't uncomment these, I"m just using them as a channel list
-    # while I'm coding, i'll delete these when I"m done with them
-    # lcm.subscribe("\auton")
-    # lcm.subscribe("\bearing")
-    # lcm.subscribe("\course")
-    # lcm.subscribe("\obstacle")
-    # lcm.subscribe("\odometry")
-    # lcm.subscribe("\tennis_ball")
+    # instantiates Simulator class
+    Simulator = SimulatorMetaClass()
 
     # constantly queries lcm server
-    lcm.subscribe("\nav_state", interface.nav_state_cb)
-    lcm.subscribe("\drive_control", interface.joystick_cb)
-
-    # testing out a method
-    # interface.sampleMethod()
+    lcm.subscribe("\nav_state", Simulator.nav_state_cb)
+    lcm.subscribe("\drive_control", Simulator.joystick_cb)
 
     # creates loop to execute this code repeatedly with the lcm
-    run_coroutines(lcm.loop(), interface.publish_bearing(lcm))
+    run_coroutines(lcm.loop(), Simulator.publish_auton_state(lcm),
+                   Simulator.publish_course(lcm),
+                   Simulator.publish_odom(lcm),
+                   Simulator.publish_obstacle(lcm),
+                   Simulator.publish_tennis_ball(lcm),
+                   simHandler.runSimulator(Simulator))
+    # as a general improvement, it may be worth threading all of the
+    # lcm-related bruhaha to offload the worst of the performance hits
+    # as the sim becomes more complex and computationally intensive
 
 
 # also necessary for the build system, idk why
